09efd3cad535ec57302242300478b47ec38190b8
stu-pksp120 <stu-pksp120@AMDVLA3511GW02>
2024-04-19 11:56:54 +0300

    Рабочая программа

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,28 +16,80 @@ namespace src
             public int term;
         }
 
-        class PlanControl //класс
+        class PlanControl
         {
-            private Subject[] subjects;
+            public Subject[] subjects;
 
-            public void CreatePlan(int size)
+
+
+            public void SortPlan()
             {
-               
+                subjects = subjects.OrderBy(s => s.term).ThenBy(s => s.name).ToArray();
                 
             }
 
-
+            public void SaveFile(string filePath)
+            {
+                using (StreamWriter file = new StreamWriter(filePath))
+                {
+                    foreach (Subject subject in subjects)
+                    {
+                        file.WriteLine($"Название: {subject.name}, Фамилия преподавателя: {subject.SurnameTeacher}, Семестр: {subject.term}");
+                    }
+                }
+            }
         }
-
-
 
         static void Main(string[] args)
         {
-            Console.Write("Введите количество значений: ");
-            int size = Int32.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Введите количество значений: ");
+                int size = Int32.Parse(Console.ReadLine());
+                 if (size==0 || size <0)
+                    {
+                    Console.WriteLine("Введите число боьше 0");
+                }
 
-            PlanControl planControl = new PlanControl();
-            
+                PlanControl planControl = new PlanControl();
+                planControl.subjects = new Subject[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"Введите {i + 1} значение:");
+                    Console.Write("Название предмета: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Фамилия преподавателя: ");
+                    string SurnameTeacher = Console.ReadLine();
+                    try
+                    {
+                        Console.Write("Семестр: ");
+                        int term = Int32.Parse(Console.ReadLine());
+                        if (term == 0 || term < 0)
+                        {
+                            Console.WriteLine("Введите число боьше 0");
+                        }
+                        planControl.subjects[i] = new Subject 
+                        { name = name, SurnameTeacher = SurnameTeacher, term = term };
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Введите целое число");
+                    }
+
+                }
+
+
+                planControl.SortPlan();
+                planControl.SaveFile("Subject.txt");
+
+                Console.WriteLine("Отсортированный массив сохранен в файл");
+            }
+            catch
+            {
+
+                Console.WriteLine("Введите целое число");
+            }
         }
     }
 }
