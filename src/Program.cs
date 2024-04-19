ec0eee41613ccc960ea8cdecb12897995de4ef65
stu-pksp120 <stu-pksp120@AMDVLA3511GW02>
2024-04-19 09:17:38 +0300

    Создание класса PlanControl

--- src/Program.cs
+++ src/Program.cs
@@ -8,16 +8,35 @@ namespace src
 {
     class Program
     {
-        struct Subject
+        struct Subject //структура
         {
             public string name;
             public string SurnameTeacher;
             public int term;
-
-           
         }
+
+        class PlanControl //класс
+        {
+            private Subject[] subjects;
+
+            public void CreatePlan(int size)
+            {
+               
+                
+            }
+
+
+        }
+
+
+
         static void Main(string[] args)
         {
+            Console.Write("Введите количество значений: ");
+            int size = Int32.Parse(Console.ReadLine());
+
+            PlanControl planControl = new PlanControl();
+            
         }
     }
 }
